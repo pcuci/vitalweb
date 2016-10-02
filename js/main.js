d2ec41e769415ca1586a69b98fc44ed5858286ac
Andrew Pomerance <andrew.pomerance@gmail.com>
2016-10-02 02:16:29 -0400

    things

--- js/main.js
+++ js/main.js
@@ -1,30 +1,59 @@
 $(document).ready(function() {
-    var exampleSocket = new WebSocket('ws://localhost:4000/echo');
-    exampleSocket.onopen = function (event) {
-        exampleSocket.send('Ping');
-    }
-    exampleSocket.onmessage = function (event) {
-        console.log("Received", event.data);
-    }
     function getData() {
         return new Promise(function(fulfill, reject) {
-            $.ajax({
-                method: 'get',
-                url: 'js/data.js',
-                success: function(res) {
-                    fulfill(JSON.parse(res));
-                },
-                error: function(res) {
-                    fulfill(JSON.parse(res.responseText));
+            $('#clinical tbody').empty();
+            $('#clinical tbody').append($('<tr><td colspan=5 align=center><br>Loading<br><img src="images/loading.gif"><br></td></tr>'));
+            setTimeout(function() {
+                $.ajax({
+                    method: 'get',
+                    url: 'js/data.js',
+                    success: function(res) {
+                        fulfill(JSON.parse(res));
+                    },
+                    error: function(res) {
+                        fulfill(JSON.parse(res.responseText));
+                    }
+                });
+            }, 2000);
+        });
+    }
+    function update() {
+        getData().then(function(data) {
+            $('#clinical tbody').empty();
+            var buttonShown = false;
+            _.each(data.data, function(visit) {
+                var row;
+                if( visit.date !== null ) {
+                    row = $('<tr><td align=center>'+visit.date+'</td><td>'+visit.procedure+'</td><td>'+visit.provider+'</td><td>'+visit.practice+'</td><td>'+visit.specialty+'</td></tr>');
+                    row.click(function() {
+                        var notes = visit.notes;
+                        var title = visit.date+' - '+visit.procedure;
+                        showPopup(notes, title);
+                    }).addClass('completed');
+                } else {
+                    if( buttonShown ) row = $('<tr><td></td><td>'+visit.procedure+'</td><td></td><td></td><td></td></tr>').addClass('future');
+                    else {
+                        row = row = $('<tr><td align=center><button>Add</button></td><td>'+visit.procedure+'</td><td></td><td></td><td></td></tr>').addClass('next');
+                        buttonShown = true;                    
+                        row.find('button').click(function() {
+                            var title = 'Upload CCD for '+visit.procedure;
+                            $('#procedure-title').text(title);
+                            $('#procedure-dialog').modal('show');
+                        });
+                    }
                 }
+                $('#clinical tbody').append(row);
             });
         });
     }
+    function doUpload() {
+        update();
+    }
+    
     function showPopup(notes, title) {
         $('#notes-title').text(title);
         $('#notes').text(notes);
         $('#notes-dialog').modal('show');
-
     };
     $('#drop-target').on('dragenter', function() {
         $('#drop-target').css('border-style', 'solid');
@@ -33,6 +62,7 @@ $(document).ready(function() {
     }).on('drop', function(ev) {
         ev.preventDefault();
         $('#procedure-dialog').modal('hide');
+        doUpload();
     });
     var stopIt = function(e) {
         e.preventDefault();
@@ -40,31 +70,5 @@ $(document).ready(function() {
         return false;
     }
     $(window).on("drag dragstart dragend dragover dragenter dragleave drop", stopIt);
-    getData().then(function(data) {
-        $('#clinical tbody').empty();
-        var buttonShown = false;
-        _.each(data.data, function(visit) {
-            var row;
-            if( visit.date !== null ) {
-                row = $('<tr><td align=center>'+visit.date+'</td><td>'+visit.procedure+'</td><td>'+visit.provider+'</td><td>'+visit.practice+'</td><td>'+visit.specialty+'</td></tr>');
-                row.click(function() {
-                    var notes = visit.notes;
-                    var title = visit.date+' - '+visit.procedure;
-                    showPopup(notes, title);
-                }).addClass('completed');
-            } else {
-                if( buttonShown ) row = $('<tr><td></td><td>'+visit.procedure+'</td><td></td><td></td><td></td></tr>').addClass('future');
-                else {
-                    row = row = $('<tr><td align=center><button>Add</button></td><td>'+visit.procedure+'</td><td></td><td></td><td></td></tr>').addClass('next');
-                    buttonShown = true;                    
-                    row.find('button').click(function() {
-                        var title = 'Upload CCD for '+visit.procedure;
-                        $('#procedure-title').text(title);
-                        $('#procedure-dialog').modal('show');
-                    });
-                }
-            }
-            $('#clinical tbody').append(row);
-        });
-    });
+    update();
 });
