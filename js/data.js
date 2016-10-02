44622a30dcac0aca473be5b5a9f08d3cfb790efb
Paul Cuciureanu <paulcu@gmail.com>
2016-10-01 21:38:42 -0400

    Adding data

--- js/data.js
+++ js/data.js
@@ -1,44 +1,14 @@
 {
   "data": [
-    [
-      "Tiger Nixon",
-      "System Architect",
-      "Edinburgh",
-      "5421",
-      "2011/04/25",
-      "$320,800"
-    ],
-    [
-      "Garrett Winters",
-      "Accountant",
-      "Tokyo",
-      "8422",
-      "2011/07/25",
-      "$170,750"
-    ],
-    [
-      "Ashton Cox",
-      "Junior Technical Author",
-      "San Francisco",
-      "1562",
-      "2009/01/12",
-      "$86,000"
-    ],
-    [
-      "Cedric Kelly",
-      "Senior Javascript Developer",
-      "Edinburgh",
-      "6224",
-      "2012/03/29",
-      "$433,060"
-    ],
-    [
-      "Airi Satou",
-      "Accountant",
-      "Tokyo",
-      "5407",
-      "2008/11/28",
-      "$162,700"
-    ]
+    {
+      date: "2011/04/25",
+      name: "Joe Ziggeler",
+      notes: "Doing great, gave new meds, alergies galore",
+      provider: "Bones Orthopaedics",
+      procedure: "Physical Therapy Evaluation",
+      practice: "Michael Bones MD",
+      specialty: "Orthopaedic surgery",
+      priority: null
+    }
   ]
 }
