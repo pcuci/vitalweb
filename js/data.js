5e86ec3d181384eb4ff987e4a7081fa1f7a3302e
Andrew Pomerance <andrew.pomerance@gmail.com>
2016-10-01 22:28:21 -0400

    popupz

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
 {
   "data": [
     {
-      date: "2011/04/25",
-      name: "Joe Ziggeler",
-      notes: "Doing great, gave new meds, alergies galore",
-      provider: "Bones Orthopaedics",
-      procedure: "Physical Therapy Evaluation",
-      practice: "Michael Bones MD",
-      specialty: "Orthopaedic surgery",
-      priority: null
+      "date": "2011/04/25",
+      "name": "Joe Ziggeler",
+      "notes": "Doing great, gave new meds, alergies galore",
+      "provider": "Bones Orthopaedics",
+      "procedure": "Physical Therapy Evaluation",
+      "practice": "Michael Bones MD",
+      "specialty": "Orthopaedic surgery",
+      "priority": null
     }
   ]
 }
--- js/main.js
+++ js/main.js
@@ -1,5 +1,38 @@
 $(document).ready(function() {
-  $('#clinical').DataTable( {
-    "ajax": 'js/data.js'
-  });
+    function getData() {
+        return new Promise(function(fulfill, reject) {
+            $.ajax({
+                method: 'get',
+                url: 'js/data.js',
+                success: function(res) {
+                    fulfill(JSON.parse(res));
+                },
+                error: function(res) {
+                    fulfill(JSON.parse(res.responseText));
+                }
+            });
+        });
+    }
+    function showPopup(notes, title) {
+        $('#notes-title').text(title);
+        $('#notes').text(notes);
+        $('#notes-dialog').modal('show');
+        
+    };
+    getData().then(function(data) {
+        $('#clinical tbody').empty();
+        _.each(data.data, function(visit) {
+            if( visit.date !== null ) {
+                var row = $('<tr><td>'+visit.date+'</td><td>'+visit.procedure+'</td><td>'+visit.provider+'</td><td>'+visit.practice+'</td><td>'+visit.specialty+'</td></tr>');
+                row.click(function() {
+                    var notes = visit.notes;
+                    var title = visit.date+' - '+visit.procedure
+                    showPopup(notes, title);
+                });
+            } else {
+                var row = $('<tr><td></td><td>'+visit.procedure+'</td><td></td><td></td><td></td></tr>');
+            }
+            $('#clinical tbody').append(row);
+        });
+    });
 });
